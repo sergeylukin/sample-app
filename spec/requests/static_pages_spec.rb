db22b64b2b42e0ad965046cf719a031c98ee1695
Sergey Lukin <contact@sergeylukin.com>
2012-09-01 03:00:10 -0400

    Test pages title in static pages integration test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,10 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_selector 'h1', text: 'Sample App'
     end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector 'title', text: 'Home | SampleApp'
+    end
   end
 
   describe "Help page" do
@@ -14,12 +18,20 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_selector 'h1', text: 'Help'
     end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector 'title', text: 'Help | SampleApp'
+    end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector 'h1', text: 'About Us'
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector 'title', text: 'About Us | SampleApp'
     end
   end
 
