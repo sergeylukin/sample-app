dbf4e9e614074885ec361895f9c7c6c06db5951c
Sergey Lukin <contact@sergeylukin.com>
2012-09-03 18:51:17 -0400

    Refactor utilities.rb in spec support

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,1 @@
-# Returns the full title on a per-page basis
-def full_title(page_title)
-  base_title = 'SampleApp'
-  if page_title.empty?
-    base_title
-  else
-    "#{page_title} | #{base_title}"
-  end
-end
-
+include ApplicationHelper
